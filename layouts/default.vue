90f242c9d03a0004ba95e7a3715ab27b798289bf
Ian Rees <ian@ianrees.net>
2025-02-15 01:28:51 -0800

    Nuxt project skeleton

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="outer">
+    <tlnav />
+
+    <div class="main">
+      <div class="container">
+        <tl-breadcrumbs
+          :boxed="false"
+        />
+
+        <slot />
+      </div>
+    </div>
+    <tlfooter />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `Playground • ${titleChunk}` : 'Playground'
+  }
+})
+</script>
+
+<style scoped>
+.main {
+  flex:1;
+}
+.footer {
+  padding:20px;
+}
+
+.outer {
+  display:flex;
+  min-height:100vh;
+  flex-direction:column;
+}
+
+.main {
+  flex:1;
+}
+
+.main {
+  flex:1;
+  padding-top: 20px;
+  padding-left:20px;
+  padding-right:20px;
+  padding-bottom:20px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,78 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export default defineNuxtConfig({
+  ssr: false,
+
+  modules: [
+    'tlv2-ui', // <-- comment out this line when using a local tlv2-ui repo
+    '@nuxt/devtools'
+  ],
+
+  devtools: {
+    enabled: isDev
+  },
+
+  tlv2: {
+    bulma: '~/assets/main.scss',
+    useProxy: true
+  },
+
+  runtimeConfig: {
+    proxyBase: '',
+    allowedReferer: '',
+    graphqlApikey: '',
+    public: {
+      apiBase: '',
+      protomapsApikey: '',
+      nearmapsApikey: '',
+      auth0Domain: '',
+      auth0ClientId: '',
+      auth0RedirectUri: '',
+      auth0Audience: '',
+      auth0Scope: '',
+      loginGate: ''
+    }
+  },
+
+  // bugs
+  build: {
+    transpile: [
+      'tslib', // https://github.com/nuxt/nuxt/issues/19265#issuecomment-1702014262
+      '@vue/apollo-composable',
+      '@apollo/client',
+      'protomaps-themes-base'
+    ]
+  },
+
+  vite: {
+    // https://github.com/nuxt/nuxt/issues/20001
+    resolve: {
+      preserveSymlinks: true
+    },
+    // bug https://github.com/apollographql/apollo-client/issues/9756
+    define: {
+      __DEV__: isDev.toString()
+    },
+    // bug https://github.com/nuxt/nuxt/issues/13247
+    optimizeDeps: {
+      include: [
+        'zen-observable',
+        'fast-json-stable-stringify',
+        'maplibre-gl',
+        'haversine',
+        '@mapbox/mapbox-gl-draw',
+        'cytoscape',
+        'mixpanel-browser'
+      ]
+    },
+    server: {
+      fs: {
+        allow: ['/Users/irees/src/interline-io', '/Users/drew/code/interline-io']
+      }
+    }
+  },
+
+  compatibilityDate: '2025-02-11'
+})
