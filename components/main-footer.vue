d5344c4a8f81bc26fffa2abdb9fba85ac9f21b14
Ian Rees <ian@ianrees.net>
2025-02-18 04:41:30 -0800

    Footer tweaks

--- components/main-footer.vue
+++ components/main-footer.vue
@@ -1,11 +1,13 @@
 <template>
   <footer class="footer">
     <div class="columns content is-small">
-      <img class="logo" src="~assets/interline-logo-small.png" alt="Interline">
-    </div>
-    <div class="column has-text-right">
-      <p>Created by <a target="_blank" href="https://www.interline.io">Interline Technologies</a>.</p>
-      <p>Map Data: <a target="_blank" href="https://www.transit.land">Transitland</a> | <a target="_blank" href="http://openstreetmap.org/copyright">&copy; OpenStreetMap</a> | <a target="_blank" href="https://carto.com/attributions">&copy; CARTO</a></p>
+      <div class="column">
+        <img class="logo" src="~assets/interline-logo-small.png" alt="Interline">
+      </div>
+      <div class="column has-text-right">
+        <p>Created by <a target="_blank" href="https://www.interline.io">Interline Technologies</a>.</p>
+        <p>Map Data: <a target="_blank" href="https://www.transit.land">Transitland</a> | <a target="_blank" href="http://openstreetmap.org/copyright">&copy; OpenStreetMap</a> | <a target="_blank" href="https://carto.com/attributions">&copy; CARTO</a></p>
+      </div>
     </div>
   </footer>
 </template>
@@ -14,8 +16,8 @@
   footer {
     margin-top:20px;
     padding: 1.5em;
-.logo {
-    max-height: 40px;
+    .logo {
+      max-height: 30px;
+    }
   }
-}
   </style>
