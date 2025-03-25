4ac7580ded68aa68edda9cc89f2999f6c11550cb
Ian Rees <ian@ianrees.net>
2025-03-25 02:57:01 -0700

    Differentiate between route-frequency and stop-frequency

--- components/cal/map.vue
+++ components/cal/map.vue
@@ -226,13 +226,27 @@ interface Matcher {
 // Depending on the data display, set up matcher rules to choose a styling.
 // Matchers should run in the order that they are added to the rules array.
 const styleData = computed((): Matcher[] => {
-  const rules: Matcher[] = []
-
-  const routeHeadwayLookup = new Map<number, number>()
+  const routeLookup = new Map<number, Route>()
   for (const route of props.routeFeatures) {
-    routeHeadwayLookup.set(route.id, route.average_frequency)
+    routeLookup.set(route.id, route)
   }
 
+  const stopLookup = new Map<number, Stop>()
+  for (const stop of props.stopFeatures) {
+    stopLookup.set(stop.id, stop)
+  }
+
+  const routeStopLookup = new Map<number, number[]>()
+  for (const stop of props.stopFeatures) {
+    for (const rs of stop.route_stops) {
+      const rid = rs.route.id
+      const stops = routeStopLookup.get(rid) || []
+      stops.push(stop.id)
+      routeStopLookup.set(rid, stops)
+    }
+  }
+
+  // Style based on AGENCY
   function getAgencyMatcher (val: string): MatchFunction {
     return (v: any) => {
       if (v.__typename === 'Stop') {
@@ -244,6 +258,7 @@ const styleData = computed((): Matcher[] => {
     }
   }
 
+  // Style based on ROUTE MODE
   function getModeMatcher (val: number): MatchFunction {
     return (v: any) => {
       if (v.__typename === 'Stop') {
@@ -255,62 +270,116 @@ const styleData = computed((): Matcher[] => {
     }
   }
 
-  function getFrequencyMatcher (val: number): MatchFunction {
+  // Style based on ROUTE FREQUENCY
+  function getRouteFrequencyMatcher (val: number): MatchFunction {
     return (v: any) => {
       if (v.__typename === 'Stop') {
         return (v as Stop).route_stops.some((rs: any) => {
-          const route = rs.route
-          const headway = routeHeadwayLookup.get(route.id) || -1
-          if (headway <= 0) {
-            return false
-          }
+          const route = routeLookup.get(rs.route.id)
+          const headway = route?.average_frequency || -1
           return headway >= val * 60
         })
       } else if (v.__typename === 'Route') {
         const headway = (v as Route).average_frequency
-        if (headway <= 0) {
-          return false
-        }
         return headway >= val * 60
       }
       return false
     }
   }
 
-  // Reserve an extra color for "other", if needed
-  const maxColor = colors.length - 1
-  let valueCount = 0
+  // Style based on STOP VISIT COUNT
+  function getStopVisitMatcher (val: number): MatchFunction {
+    return (v: any) => {
+      if (v.__typename === 'Stop') {
+        const count = (v as Stop).visits?.total.visit_average || -1
+        return count >= val
+      } else if (v.__typename === 'Route') {
+        const stopIds = routeStopLookup.get((v as Route).id) || []
+        return stopIds.some((sid) => {
+          const stop = stopLookup.get(sid)
+          return (stop?.visits?.total.visit_average || -1) >= val
+        })
+      }
+      return false
+    }
+  }
 
-  // Fares not implemented yet, for now just style Fares as agencies
-  if (props.dataDisplayMode === 'Agency' || props.colorKey === 'Fares') {
+  // Generate a set of AGENCY MATCHERS
+  function getAgencyMatchers (): Matcher[] {
+    const rules: Matcher[] = []
     const agencies = agencyData.value || []
-    valueCount = agencies.length
-    for (let i = 0; i < Math.min(valueCount, maxColor); i++) {
+    for (let i = 0; i < Math.min(agencies.length, maxColor); i++) {
       const agency = agencies[i]
       const color = colors[i]
       rules.push({ label: agency.name, color: color, match: getAgencyMatcher(agency.id) })
     }
-  } else if (props.colorKey === 'Mode') {
+    return rules
+  }
+
+  // Generate a set of MODE MATCHERS (static)
+  function getModeMatchers (): Matcher[] {
+    const rules: Matcher[] = []
     const modes = [...routeTypes.keys()]
-    valueCount = modes.length
-    for (let i = 0; i < Math.min(valueCount, maxColor); i++) {
+    for (let i = 0; i < Math.min(modes.length, maxColor); i++) {
       const mode = modes[i]
       const label = routeTypes.get(mode) || 'Unknown'
       const color = colors[i]
       rules.push({ label: label, color: color, match: getModeMatcher(mode) })
     }
-  } else if (props.colorKey === 'Frequency') {
-    valueCount = 5
-    rules.push({ label: '40+', color: colors[0], match: getFrequencyMatcher(40) })
-    rules.push({ label: '30-39', color: colors[1], match: getFrequencyMatcher(30) })
-    rules.push({ label: '20-29', color: colors[2], match: getFrequencyMatcher(20) })
-    rules.push({ label: '10-19', color: colors[3], match: getFrequencyMatcher(10) })
-    rules.push({ label: '0-9', color: colors[4], match: getFrequencyMatcher(0) })
+    return rules
+  }
+
+  // Generate a set of ROUTE FREQUENCY MATCHERS (static)
+  function getRouteFrequencyMatchers (): Matcher[] {
+    const rules: Matcher[] = []
+    rules.push({ label: '40+ mins', color: colors[0], match: getRouteFrequencyMatcher(40) })
+    rules.push({ label: '30-39 mins', color: colors[1], match: getRouteFrequencyMatcher(30) })
+    rules.push({ label: '20-29 mins', color: colors[2], match: getRouteFrequencyMatcher(20) })
+    rules.push({ label: '10-19 mins', color: colors[3], match: getRouteFrequencyMatcher(10) })
+    rules.push({ label: '0-9 mins', color: colors[4], match: getRouteFrequencyMatcher(0) })
     rules.push({ label: 'Unknown', color: '#000', match: x => true })
+    return rules
+  }
+
+  // Generate a set of STOP VISIT MATCHERS (static)
+  function getStopVisitMatchers (): Matcher[] {
+    const rules: Matcher[] = []
+    rules.push({ label: '100+ visits', color: colors[0], match: getStopVisitMatcher(100) })
+    rules.push({ label: '50-100 visits', color: colors[1], match: getStopVisitMatcher(50) })
+    rules.push({ label: '20-50 visits', color: colors[2], match: getStopVisitMatcher(20) })
+    rules.push({ label: '10-20 visits', color: colors[3], match: getStopVisitMatcher(10) })
+    rules.push({ label: '0-9 visits', color: colors[4], match: getStopVisitMatcher(0) })
+    rules.push({ label: 'Unknown', color: '#000', match: x => true })
+    return rules
+  }
+
+  // Reserve an extra color for "other", if needed
+  const maxColor = colors.length - 1
+  const rules: Matcher[] = []
+
+  // Seven modes
+  if (props.dataDisplayMode === 'Agency') {
+    rules.push(...getAgencyMatchers())
+  } else if (props.dataDisplayMode === 'Route') {
+    if (props.colorKey === 'Mode') {
+      rules.push(...getModeMatchers())
+    } else if (props.colorKey === 'Frequency') {
+      rules.push(...getRouteFrequencyMatchers())
+    } else if (props.colorKey === 'Fares') {
+      // not implemented
+    }
+  } else if (props.dataDisplayMode === 'Stop') {
+    if (props.colorKey === 'Mode') {
+      rules.push(...getModeMatchers())
+    } else if (props.colorKey === 'Frequency') {
+      rules.push(...getStopVisitMatchers())
+    } else if (props.colorKey === 'Fares') {
+      // not implemented
+    }
   }
 
   // If we used all colors (or no colors), add a catchall "other" rule
-  if (valueCount > maxColor || valueCount === 0) {
+  if (rules.length >= maxColor || rules.length === 0) {
     rules.push({ label: 'Other', color: '#000', match: v => true })
   }
 
