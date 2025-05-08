6f3db7f3c0608dbfac7c4ad8cf0c7cdf9a359f29
Bryan Housel <bhousel@gmail.com>
2025-05-07 22:40:26 -0400

    Add fields for selecting admin boundary type and boundaries
    - `geomSource` select field now supports "Administrative Boundary" option
    - Added `boundaryType` select field and code to persist to url querystring
    - Added "Include Boundaries" Oruga tagInput field for the boundary picker
    - Added some sample census boundaries to make the tagInput field work
    - These fields are only visible when geomsSource === adminBoundary
    - These are on the existing query.vue component for now

--- components/cal/query.vue
+++ components/cal/query.vue
@@ -34,6 +34,26 @@
       </tl-msg-box>
 
       <tl-msg-box variant="text" title="Geographic Bounds">
+        <tl-msg-warning v-if="debugMenu" class="mt-4" style="width:400px" title="Debug menu">
+          <o-field label="Preset bounding box">
+            <o-select v-model="cannedBbox">
+              <option v-for="cannedBboxName of cannedBboxes.keys()" :key="cannedBboxName" :value="cannedBboxName">
+                {{ cannedBboxName }}
+              </option>
+            </o-select>
+          </o-field>
+          <br>
+          <o-field label="Data options">
+            <o-checkbox
+              v-model="scheduleEnabled"
+              :true-value="true"
+              :false-value="false"
+            >
+              Load schedule data
+            </o-checkbox>
+          </o-field>
+        </tl-msg-warning>
+
         <div class="columns">
           <div class="column is-half">
             <o-field>
@@ -43,52 +63,49 @@
                   <o-icon icon="information" />
                 </o-tooltip>
               </template>
-              <o-select v-model="geomSource">
-                <option value="mapExtent" selected>
-                  Covering extent of map
-                </option>
-                <option value="bbox">
-                  Dragging bounding box
-                </option>
-              </o-select>
+              <o-select
+                v-model="geomSource"
+                :options="geomSources"
+                @input="changeGeomSource"
+              />
+            </o-field>
+          </div>
 
-              <tl-msg-warning v-if="debugMenu" class="mt-4" style="width:400px" title="Debug menu">
-                <o-field label="Preset bounding box">
-                  <o-select v-model="cannedBbox">
-                    <option v-for="cannedBboxName of cannedBboxes.keys()" :key="cannedBboxName" :value="cannedBboxName">
-                      {{ cannedBboxName }}
-                    </option>
-                  </o-select>
-                </o-field>
-                <br>
-                <o-field label="Data options">
-                  <o-checkbox
-                    v-model="scheduleEnabled"
-                    :true-value="true"
-                    :false-value="false"
-                  >
-                    Load schedule data
-                  </o-checkbox>
-                </o-field>
-              </tl-msg-warning>
-
-              <!-- <div class="cal-bbox-info">
-                <div style="text-align:center">
-                  <o-button icon-left="pin">
-                    Select on map
-                  </o-button>
-                </div>
-                <o-field label="SW Corner">
-                  <o-input type="text" :value="ptString(props.bbox.sw)" />
-                </o-field>
-                <o-field label="NE Corner">
-                  <o-input type="text" :value="ptString(props.bbox.ne)" />
-                </o-field>
-              </div> -->
+          <div class="column is-half" :class="{ 'is-hidden': geomSource !== 'adminBoundary' }">
+            <o-field>
+              <template #label>
+                Boundary Type
+              </template>
+              <o-select
+                v-model="boundaryType"
+                :options="boundaryTypes"
+                @input="changeBoundaryType"
+              />
             </o-field>
           </div>
         </div>
+
+        <div class="container is-max-tablet" :class="{ 'is-hidden': geomSource !== 'adminBoundary' }">
+          <o-field>
+            <template #label>
+              Include Boundaries
+            </template>
+            <o-taginput
+              v-model="boundaries"
+              :options="sampleBoundaryData"
+              open-on-focus
+              closable
+              close-icon=""
+              keep-open
+              icon="magnify"
+              placeholder="Search..."
+              :filter="filterBoundaries"
+              expanded
+            />
+          </o-field>
+        </div>
       </tl-msg-box>
+
       <o-button variant="primary" :disabled="!validQueryParams" class="is-fullwidth is-large" @click="emit('explore')">
         Run Query
       </o-button>
@@ -98,7 +115,7 @@
 
 <script setup lang="ts">
 import { type Bbox, parseBbox } from '../geom'
-import { cannedBboxes } from '../constants'
+import { cannedBboxes, geomSources, boundaryTypes } from '../constants'
 import { useToggle } from '@vueuse/core'
 
 const props = defineProps<{
@@ -113,11 +130,36 @@ const emit = defineEmits([
 const startDate = defineModel<Date>('startDate')
 const endDate = defineModel<Date>('endDate')
 const geomSource = defineModel<string>('geomSource')
+const boundaryType = defineModel<string>('boundaryType')
 const scheduleEnabled = defineModel<boolean>('scheduleEnabled')
 const cannedBbox = ref('')
 const selectSingleDay = ref(true)
 const toggleSelectSingleDay = useToggle(selectSingleDay)
 const debugMenu = useDebugMenu()
+
+const boundaries = ref([])
+const sampleBoundaryData = {
+  Q581346: 'Aloha',
+  Q2587933: 'Barberton',
+  Q588923: 'Bethany',
+  Q1815228: 'Bull Mountain',
+  Q2896285: 'Cedar Hills',
+  Q2896288: 'Cedar Mill',
+  Q3473327: 'Dunthorpe',
+  Q3046557: 'Fairview',
+  Q1815675: 'Felida',
+  Q1983444: 'Garden Home-Whitford',
+  Q2605832: 'Hazel Dell',
+  Q1815789: 'Hockinson',
+  Q3473329: 'Marlene Village',
+  Q1815894: 'Minnehaha',
+  Q1815903: 'Mount Vista',
+  Q2889407: 'Oak Hills',
+  Q2581728: 'Orchards',
+  Q1815994: 'Salmon Creek',
+  Q3473334: 'West Haven-Sylvan',
+  Q1816149: 'West Slope',
+}
 
 watch(() => cannedBbox.value, (cannedBboxName) => {
   if (cannedBboxName) {
@@ -129,22 +171,42 @@ const validQueryParams = computed(() => {
   return startDate.value && props.bbox?.valid
 })
 
+function changeGeomSource () {
+  boundaryType.value = ''
+  boundaries.value = []
+}
+
+function changeBoundaryType () {
+  boundaries.value = []
+}
+
+// Which boundaries to show in the select field
+// @param  option - the option to test (keys from the list of boundaries)
+// @param  value  - the value to test (what the user typed)
+function filterBoundaries (option: string, value: string): boolean {
+  const exists = Array.from(boundaries.value).includes(option)
+  if (exists) {
+    return true // value was already picked, exclude it
+  }
+  const label = sampleBoundaryData[option]?.toLowerCase()
+  const val = value.toLowerCase()
+  return !label?.includes(val)
+}
+
 </script>
 
 <style scoped lang="scss">
-.cal-query {
-  display:flex;
-  flex-direction:column;
-  background: var(--bulma-scheme-main);
-  height:100%;
-  padding-left:20px;
-  padding-right:20px;
-  > .cal-body {
-      > div, > article
-       {
-      margin-bottom:10px;
-    }
-
+  .cal-query {
+    display:flex;
+    flex-direction:column;
+    background: var(--bulma-scheme-main);
+    height:100%;
+    padding-left:20px;
+    padding-right:20px;
+    > .cal-body {
+      > div, > article {
+        margin-bottom:10px;
+      }
     }
   }
 
@@ -153,4 +215,4 @@ const validQueryParams = computed(() => {
     margin-top:10px;
     padding:10px;
   }
-  </style>
+</style>
