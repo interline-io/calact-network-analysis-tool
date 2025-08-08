2748d26ca412b3ed106ef7baaa872875e1619f3f
Ian Rees <ian@ianrees.net>
2025-08-08 00:01:04 -0700

    vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['tests/**/*.test.ts', 'src/**/*.test.ts'],
+    exclude: ['node_modules', 'dist', '.nuxt']
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '.'),
+      '@': './src'
+    }
+  }
+})
