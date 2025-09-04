e0dda59e8bf5f8ddcd0d500cc1db9e239fd9b436
Ian Rees <ian@ianrees.net>
2025-09-03 18:03:37 -0700

    More debug

--- composables/useTransitlandApiEndpoint.ts
+++ composables/useTransitlandApiEndpoint.ts
@@ -1,8 +1,9 @@
 export const useTransitlandApiEndpoint = (path?: string) => {
   const config = useRuntimeConfig()
-  console.log('config?', config, 'import.meta.server', import.meta.server)
   const apiBase = import.meta.server
     ? (config.tlv2?.proxyBase)
     : (config.public.tlv2?.apiBase || window?.location?.origin + '/api/v2')
+  console.log('config?', config)
+  console.log('apiBase?', apiBase)
   return apiBase + (path || '')
 }
