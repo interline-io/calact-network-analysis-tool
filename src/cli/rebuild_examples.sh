7fdaee1c50a58a07d1717d310b4fa1531984edde
Ian Rees <ian@ianrees.net>
2026-03-18 16:10:07 -0700

    Script fixes

--- src/cli/rebuild_examples.sh
+++ src/cli/rebuild_examples.sh
@@ -5,7 +5,7 @@ export OUTDIR="public/examples"
 # Save scenario and WSDOT data
 for scenario in downtown-portland downtown-portland-zoomed bend eugene salem portland greater-seattle greater-portland; do
     rm $OUTDIR/$scenario.*
-    /usr/bin/time yarn calact scenario \
+    /usr/bin/time pnpm calact scenario \
         --bbox-name="$scenario" \
         --save-scenario-data "$OUTDIR/$scenario.json"
 done;
@@ -13,7 +13,7 @@ done;
 # Save scenario and WSDOT data
 for scenario in downtown-portland downtown-portland-zoomed bend eugene salem portland greater-seattle greater-portland; do
     rm $OUTDIR/$scenario.*
-    /usr/bin/time yarn calact wsdot \
+    /usr/bin/time pnpm calact wsdot \
         --bbox-name="$scenario" \
         --save-scenario-data "$OUTDIR/$scenario.wsdot.json"
 done;
